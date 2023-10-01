5307e95720d3e8190d9d3996935fb58485926cb7
deepak74620 <105719092+deepak74620@users.noreply.github.com>
2023-10-01 22:15:49 +0530

    Create Moore’s_Voting_Algorithm.cpp
    A program to return an element that occurs more than N/2 times in the given array.

---
+++ C++/Moore’s_Voting_Algorithm.cpp
@@ -0,0 +1,48 @@
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int majorityElement(vector<int> v) {
+
+    //size of the given array:
+    int n = v.size();
+    int cnt = 0; // count
+    int el; // Element
+
+    //applying the algorithm:
+    for (int i = 0; i < n; i++) {
+        if (cnt == 0) {
+            cnt = 1;
+            el = v[i];
+        }
+        else if (el == v[i]) cnt++;
+        else cnt--;
+    }
+
+    //checking if the stored element
+    // is the majority element:
+    int cnt1 = 0;
+    for (int i = 0; i < n; i++) {
+        if (v[i] == el) cnt1++;
+    }
+
+    if (cnt1 > (n / 2)) return el;
+    return -1;
+}
+
+int main()
+{
+    vector<int> arr = {2, 2, 1, 1, 1, 2, 2};
+    int ans = majorityElement(arr);
+    cout << "The majority element is: " << ans << endl;
+    return 0;
+}
+
+// Output: The majority element is: 2
+
+// Time Complexity: O(N) + O(N), where N = size of the given array.
+// Reason: The first O(N) is to calculate the count and find the expected majority element. The second one is to check if the expected element is the majority one or not.
+
+// Space Complexity: O(1) as we are not using any extra space.
