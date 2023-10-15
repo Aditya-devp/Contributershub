6b1ac08a6165e5f0eeadb6512412c3a2f8a69ba3
deepak74620 <105719092+deepak74620@users.noreply.github.com>
2023-10-16 00:31:27 +0530

    Create All_Sub-sequence_of_a_String.cpp
    A program to find all the possible sub sequences of a string.

---
+++ C++/All_Sub-sequence_of_a_String.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+vector<string> AllPossibleStrings(string s) {
+	int n = s.length();
+	vector<string>ans;
+	for (int num = 0; num < (1 << n); num++) {
+		string sub = "";
+		for (int i = 0; i < n; i++) {
+			//check if the ith bit is set or not
+			if (num & (1 << i)) {
+				sub += s[i];
+			}
+		}
+		if (sub.length() > 0) {
+			ans.push_back(sub);
+		}
+	}
+	sort(ans.begin(), ans.end());
+	return ans;
+}
+int main()
+{
+
+
+	string s="abc";
+	vector<string>ans = AllPossibleStrings(s);
+	//printint all the subsequence.
+	cout<<"All possible subsequences are "<<endl;
+	for (auto it : ans) {
+		cout << it << " ";
+	}
+
+}
+
+// Output:
+// All possible subsequences are
+// a ab abc ac b bc c
+
+// Time Complexity: O(2^n * n)
+// Reason: O(2^n) for the outer for loop and O(n) for the inner for loop.
+
+// Space Complexity: O(1)
