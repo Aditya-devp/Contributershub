a5c1d1dd77be85c99446703f6fcb3316a4b92288
Manas <manasverma012345@gmail.com>
2023-10-21 16:18:03 +0530

    Initial Commit

---
+++ WebD/AuthGuard/client/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Route, Routes, Navigate } from "react-router-dom";
+import Main from "./components/Main";
+import Signup from "./components/Signup";
+import Login from "./components/Login";
+import EmailVerify from "./components/EmailVerify";
+import ForgotPassword from "./components/ForgotPassword";
+import PasswordReset from "./components/PasswordReset";
+
+function App() {
+	const user = localStorage.getItem("token");
+
+	return (
+		<Routes>
+			{user && <Route path="/" exact element={<Main />} />}
+			<Route path="/signup" exact element={<Signup />} />
+			<Route path="/login" exact element={<Login />} />
+			<Route path="/" element={<Navigate replace to="/login" />} />
+			<Route path="/users/:id/verify/:token" element={<EmailVerify />} />
+			<Route path="/forgot-password" element={<ForgotPassword />} />
+			<Route path="/password-reset/:id/:token" element={<PasswordReset />} />
+		</Routes>
+	);
+}
+
+export default App;
