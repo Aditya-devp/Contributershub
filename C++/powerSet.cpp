586b5c2b1b3c26a13ab0f105d4fe2b5e0f1f970a
Lakshay Kumar <2004lky@gmail.com>
2023-10-29 21:37:21 +0530

    Update powerSet.cpp

--- C++/powerSet.cpp
+++ C++/powerSet.cpp
@@ -2,7 +2,6 @@
 Description of code
 
 Given an integer array nums that may contain duplicates, return all possible subsets (the power set).
-
 The solution set must not contain duplicate subsets. Return the solution in any order
 
 */
