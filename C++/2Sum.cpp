9660e59cbbe4fd391e923fd66c3a2f4e719beb97
Divyata Pandey <101250441+divyatapandey@users.noreply.github.com>
2023-10-31 21:18:11 +0530

    Update 2Sum.cpp

--- C++/2Sum.cpp
+++ C++/2Sum.cpp
@@ -1,41 +1,15 @@
-" Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
-  You may assume that each input would have exactly one solution, and you may not use the same element twice.
-  You can return the answer in any order.
-  Contributed by: Anuj Singh Kushwaha (@whyanujjwhy)
-"
-  
-#include <bits/stdc++.h>
-using namespace std;
-
-vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int> res;
+vector<int> twoSum(vector<int>& nums, int target) 
+{
+        //created a map to store numbers with their indices
         unordered_map<int, int> mp;
-        for(int i=0; i<nums.size(); i++) {
-            int moreNeeded=target-nums[i];
-            if(mp.find(moreNeeded) != mp.end()) {
-                return {mp[moreNeeded], i};
-            }
-            mp[nums[i]]=i;
+       
+        for(int i = 0; i < nums.size(); i++){
+            if(mp.find(target - nums[i]) == mp.end())
+                mp[nums[i]] = i;
+            else
+                return {mp[target - nums[i]], i};
         }
+       //returning {-1,-1} if not found
         return {-1, -1};
-    }
-int main() {
-    int n;
-    cout<<"Enter size of array: ";
-    cin>>n;
-
-    vector<int> v(n);
-    for(int i=0; i<n; i++) {
-        cout<<"\nEnter element "<<i+1<<" ";
-        cin>>v[i];
-    }
-    int target;
-    cout<<"Enter target value to find in pair: ";
-    cin>>target;
-
-    vector<int> ans;
-    ans=twoSum(v, target);
-    cout<<"Pair resultig in target value: "<<ans[0]<<" "<<ans[1];
-
-    return 0;
-}
+} 
+    
